c146246e4db4af38f97bf8e5830a641b9bde8676
Zsolt Molnár <mozb17@gmail.com>
2019-11-05 17:09:09 +0100

    Implement IdentifyImageAsync().

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -34,6 +36,35 @@ namespace WpfWithRestApi
             if (fileDialog.ShowDialog() == true)
             {
                 string filename = fileDialog.FileName;
+
+                selectedImage.Source = new BitmapImage(new Uri(filename));
+
+                IdentifyImageAsync(filename);
+            }
+        }
+
+        private async void IdentifyImageAsync(string filename)
+        {
+            var file = File.ReadAllBytes(filename);
+
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/546b82b7-a125-47c7-8270-243f841b4bb4/classify/iterations/Iteration1/image";
+            string predictionKey = "ac088cbe67dc4946baf2eb394e1675d1";
+            string contentType = "application/octet-stream";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+
+
+                using (var content = new ByteArrayContent(file))
+                {
+
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+
+                    var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+                }
             }
         }
     }
